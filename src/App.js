3bba34775c15692c44ec2622fd32902af2ba4cc8
Pearl-15 <naruto.pk15@gmail.com>
2023-09-06 17:23:19 +0800

    implement Stopwatch and App class component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import './App.css';
 import Stopwatch from './components/Stopwatch';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <Stopwatch />
-    </div>
-  );
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      showStopWatch: true
+    }
+  }
+
+  render(){
+    return(
+      <div>
+        <label>Show Stop Watch{' '}
+        <input
+          type="checkbox"
+          checked={this.state.showStopWatch}
+          onChange={()=>{
+            this.setState({showStopWatch: !this.state.showStopWatch})
+          }}
+          />
+      </label>
+
+      <hr />
+      {this.state.showStopWatch ? <Stopwatch /> : null}
+
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -1,8 +1,52 @@
 import React from 'react';
 
 class Stopwatch extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            lapse:0,
+            running: false
+        }
+    }
+
+    hanldeRunCilck(){
+
+        const startTime = Date.now() - this.state.lapse;
+
+        if (this.state.running){
+            clearInterval(this.timer);
+        }else{
+            this.timer = setInterval(()=>{
+                this.setState({lapse: Date.now() - startTime}, ()=>{
+                    //callback function
+                    console.log(this.state.lapse)
+                });
+
+            })          
+        }
+        this.setState({running: !this.state.running});       
+    }
+
+    handleClearClick(){
+        clearInterval(this.timer);
+        this.setState({lapse: 0, running: false});
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.timer);
+    }
+
     render(){
-        return (<h1> This is Stop Watch</h1>)
+
+        return (
+        <div>
+            <h1> This is Stop Watch</h1>
+            <label>{this.state.lapse}ms</label>
+            <button onClick={()=> this.hanldeRunCilck()}>{ this.state.running ? 'Stop' : 'Start'}</button>
+            <button onClick={()=> this.handleClearClick()}>Clear</button>
+        </div>
+        )
     }
 }
 
